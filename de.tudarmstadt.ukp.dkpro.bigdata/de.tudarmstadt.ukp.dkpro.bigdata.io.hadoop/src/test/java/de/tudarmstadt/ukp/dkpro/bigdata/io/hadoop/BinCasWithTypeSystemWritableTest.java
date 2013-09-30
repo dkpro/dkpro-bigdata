74e44a4b6ef157ecbd44beae220966e9e69e3dc1
Hans-Peter Zorn <zorn@ukp.informatik.tu-darmstadt.de>
2013-09-30 18:00:00 +0200

    adding tests for BinCas and HdfsResourceLoader

---
+++ de.tudarmstadt.ukp.dkpro.bigdata/de.tudarmstadt.ukp.dkpro.bigdata.io.hadoop/src/test/java/de/tudarmstadt/ukp/dkpro/bigdata/io/hadoop/BinCasWithTypeSystemWritableTest.java
@@ -0,0 +1,11 @@
+package de.tudarmstadt.ukp.dkpro.bigdata.io.hadoop;
+
+public class BinCasWithTypeSystemWritableTest
+    extends CASWritableTest
+{
+    public BinCasWithTypeSystemWritableTest()
+    {
+        writable = BinCasWithTypeSystemWritable.class;
+    }
+
+}

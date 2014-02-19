97f2a449addf5b4bbc1c40195f81f979e4867ac0
Hans-Peter Zorn <hzorn@inovex.de>
2014-02-19 10:13:02 +0100

    Added Metadata Extraction to Text2CASInputFormat

--- de.tudarmstadt.ukp.dkpro.bigdata/de.tudarmstadt.ukp.dkpro.bigdata.io.hadoop/src/main/java/de/tudarmstadt/ukp/dkpro/bigdata/io/hadoop/Text2CASInputFormat.java
+++ de.tudarmstadt.ukp.dkpro.bigdata/de.tudarmstadt.ukp.dkpro.bigdata.io.hadoop/src/main/java/de/tudarmstadt/ukp/dkpro/bigdata/io/hadoop/Text2CASInputFormat.java
@@ -34,35 +34,35 @@ import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
 /**
  * Input format for generating CAS instances from <Text, Text> key/value pairs
  * 
- * By default, the value of the <Text, Text> key/value lines in the input files is used as CAS
- * document text. See {@link setDocumentTextExtractorClass} to change this behavior.
+ * By default, the value of the <Text, Text> key/value lines in the input files
+ * is used as CAS document text. See {@link setDocumentTextExtractorClass} to
+ * change this behavior.
  * 
  * @author Johannes Simon
  * 
  */
-public class Text2CASInputFormat
-extends FileInputFormat<Text, CASWritable>
-{
+public class Text2CASInputFormat extends FileInputFormat<Text, CASWritable> {
 
 	/**
-	 * Provide a custom implementation of this interface if you want the generated CAS instances to
-	 * contain text different from the value of <Text, Text> key/value lines in the input files.<br>
+	 * Provide a custom implementation of this interface if you want the
+	 * generated CAS instances to contain text different from the value of
+	 * <Text, Text> key/value lines in the input files.<br>
 	 * <br>
-	 * This is useful e.g. if you want to remove HTML markup or if you want to extract text from the
-	 * <b>key</b>, not from the <b>value</b>.<br>
+	 * This is useful e.g. if you want to remove HTML markup or if you want to
+	 * extract text from the <b>key</b>, not from the <b>value</b>.<br>
 	 * <br>
-	 * By default, i.e. if you do not set a custom DocumentTextExtractor, the input line's value is
-	 * used as the CAS document text.
+	 * By default, i.e. if you do not set a custom DocumentTextExtractor, the
+	 * input line's value is used as the CAS document text.
 	 */
-	public interface DocumentTextExtractor
-	{
+	public interface DocumentTextExtractor {
 		Text extractDocumentText(Text key, Text value);
 	}
 
 	/**
-	 * Tells Text2CASInputFormat to use a custom implementation of DocumentTextExtractor. <br>
-	 * By default, i.e. if you do not set a custom DocumentTextExtractor, the input line's value is
-	 * used as the CAS document text.
+	 * Tells Text2CASInputFormat to use a custom implementation of
+	 * DocumentTextExtractor. <br>
+	 * By default, i.e. if you do not set a custom DocumentTextExtractor, the
+	 * input line's value is used as the CAS document text.
 	 * 
 	 * @param conf
 	 *            Configuration object
@@ -70,58 +70,89 @@ extends FileInputFormat<Text, CASWritable>
 	 *            Implementation of DocumentTextExtractor
 	 */
 	public static void setDocumentTextExtractorClass(Configuration conf,
-			Class<? extends DocumentTextExtractor> extractorClass)
-	{
-		conf.set("dkpro.uima.text2casinputformat.documenttextextractor", extractorClass.getName());
+			Class<? extends DocumentTextExtractor> extractorClass) {
+		conf.set("dkpro.uima.text2casinputformat.documenttextextractor",
+				extractorClass.getName());
 	}
 
 	/**
-	 * Reads in CAS from XML or plain text.<br/>
+	 * Provide a custom implementation of this interface if you want the
+	 * generated CAS instances to contain text different from the value of
+	 * <Text, Text> key/value lines in the input files.<br>
+	 * <br>
+	 * This is useful e.g. if you want to remove HTML markup or if you want to
+	 * extract text from the <b>key</b>, not from the <b>value</b>.<br>
+	 * <br>
+	 * By default, i.e. if you do not set a custom DocumentTextExtractor, the
+	 * input line's value is used as the CAS document text.
+	 */
+	public interface DocumentMetadataExtractor {
+		void extractDocumentMetaData(Text key, Text value,
+				DocumentMetaData metadata);
+	}
+
+	/**
+	 * Tells Text2CASInputFormat to use a custom implementation of
+	 * DocumentTextExtractor. <br>
+	 * By default, i.e. if you do not set a custom DocumentTextExtractor, the
+	 * input line's value is used as the CAS document text.
+	 * 
+	 * @param conf
+	 *            Configuration object
+	 * @param extractorClass
+	 *            Implementation of DocumentTextExtractor
+	 */
+	public static void setDocumentMetadataExtractorClass(Configuration conf,
+			Class<? extends DocumentMetadataExtractor> extractorClass) {
+		conf.set("dkpro.uima.text2casinputformat.documentmetadataextractor",
+				extractorClass.getName());
+	}
+
+	/**
+	 * Reads in CAS from JSON, XML or plain text.<br/>
 	 * <br/>
 	 * This is an internal class. It is only visible to hadoop.io package.
 	 * 
 	 * @author Johannes Simon
 	 * 
 	 */
-	private class Text2CASRecordReader
-	extends GenericKeyValueLineRecordReader<Text, CASWritable>
-	{
+	private class Text2CASRecordReader extends
+			GenericKeyValueLineRecordReader<Text, CASWritable> {
 		private final DocumentTextExtractor textExtractor;
+		private final DocumentMetadataExtractor metadataExtractor;
 
-		public Text2CASRecordReader(FileSplit fileSplit, JobConf jobConf, Reporter reporter,
-				DocumentTextExtractor textExtractor)
-						throws IOException
-						{
+		public Text2CASRecordReader(FileSplit fileSplit, JobConf jobConf,
+				Reporter reporter, DocumentTextExtractor textExtractor,
+				DocumentMetadataExtractor metadataExtractor) throws IOException {
 			super(fileSplit, jobConf, reporter);
 			this.textExtractor = textExtractor;
-						}
+			this.metadataExtractor = metadataExtractor;
+		}
 
 		@Override
-		public Text createKey()
-		{
+		public Text createKey() {
 			return new Text();
 		}
 
 		@Override
-		public CASWritable createValue()
-		{
+		public CASWritable createValue() {
 			return new CASWritable();
 		}
 
 		@Override
-		public void convertKey(Text keyFrom, Text valueFrom, Text keyTo)
-		{
+		public void convertKey(Text keyFrom, Text valueFrom, Text keyTo) {
 			keyTo.set(keyFrom);
 		}
 
 		@Override
-		public void convertValue(Text keyFrom, Text valueFrom, CASWritable valueTo)
-		{
+		public void convertValue(Text keyFrom, Text valueFrom,
+				CASWritable valueTo) {
 			CAS cas = valueTo.getCAS();
 			cas.reset();
 			Text doc = valueFrom;
 			if (textExtractor != null)
 				doc = textExtractor.extractDocumentText(keyFrom, valueFrom);
+
 			cas.setDocumentText(doc.toString());
 			try {
 				// add some simple metadata
@@ -129,7 +160,11 @@ extends FileInputFormat<Text, CASWritable>
 				String key_abbrev = StringUtils.abbreviate(key_as_str, 50);
 				DocumentMetaData metadata = DocumentMetaData.create(cas);
 				metadata.setDocumentTitle(key_abbrev);
-				metadata.setDocumentId(String.format("<%d>%s", key_as_str.hashCode(), key_abbrev));
+				metadata.setDocumentId(String.format("<%d>%s",
+						key_as_str.hashCode(), key_abbrev));
+				if (metadataExtractor != null)
+					metadataExtractor.extractDocumentMetaData(keyFrom,
+							valueFrom, metadata);
 			} catch (Exception e) {
 				System.err.println("DocumentMetaData already present.");
 			}
@@ -138,29 +173,40 @@ extends FileInputFormat<Text, CASWritable>
 	}
 
 	@Override
-	public RecordReader<Text, CASWritable> getRecordReader(InputSplit split, JobConf jobConf,
-			Reporter reporter)
-					throws IOException
-					{
+	public RecordReader<Text, CASWritable> getRecordReader(InputSplit split,
+			JobConf jobConf, Reporter reporter) throws IOException {
 		DocumentTextExtractor textConverter = null;
 		String textConverterClass = jobConf
 				.get("dkpro.uima.text2casinputformat.documenttextextractor");
 		if (textConverterClass != null) {
 			try {
-				textConverter = (DocumentTextExtractor) Class.forName(textConverterClass)
-						.newInstance();
-			}
-			catch (InstantiationException e) {
+				textConverter = (DocumentTextExtractor) Class.forName(
+						textConverterClass).newInstance();
+			} catch (InstantiationException e) {
 				e.printStackTrace();
-			}
-			catch (IllegalAccessException e) {
+			} catch (IllegalAccessException e) {
 				e.printStackTrace();
-			}
-			catch (ClassNotFoundException e) {
+			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
 			}
 		}
-		return new Text2CASRecordReader((FileSplit) split, jobConf, reporter, textConverter);
-					}
+		DocumentMetadataExtractor metadataConverter = null;
+		String metadataConverterClass = jobConf
+				.get("dkpro.uima.text2casinputformat.documentmetadataextractor");
+		if (metadataConverterClass != null) {
+			try {
+				metadataConverter = (DocumentMetadataExtractor) Class.forName(
+						metadataConverterClass).newInstance();
+			} catch (InstantiationException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+		return new Text2CASRecordReader((FileSplit) split, jobConf, reporter,
+				textConverter, metadataConverter);
+	}
 
 }
